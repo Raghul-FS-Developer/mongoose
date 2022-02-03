8d604787847646d75a4dce28ce292965611e380e
Raghul <88141940+princeraghul@users.noreply.github.com>
2022-02-03 14:02:51 +0530

    second commit

--- routes/users.js
+++ routes/users.js
@@ -1,103 +1,120 @@
 var express = require('express');
-const req = require('express/lib/request');
-const { ObjectId } = require('mongodb');
 var router = express.Router();
-const {dburl,mongodb,MongoClient}= require('../dbConfig')
-/* GET users listing. */
-router.get('/all',async(req, res)=>{
-const client = await MongoClient.connect(dburl);
-try {
-  const db = await client.db('b26we');
-  let document = await db.collection('users').find().toArray()
+const {dbUrl,mongodb,MongoClient} = require('../dbConfig');
 
-  res.json({
-    message:"Data Fetched Successfully",
-    data:document
-  })
-} catch (error) {
-    console.log(error)
-    res.json({
-      message:"data fetching failed"
-    })  
-} finally{
-  client.close()
-}
+router.get('/all',async(req,res)=>{
+  const client = await MongoClient.connect(dbUrl);
+    try {
+      const db = await client.db('b26we');
+      let document = await db.collection('users').find().toArray();
+
+      res.json({
+        message:"Data fetched Successfully!",
+        data:document
+      })
+
+    } catch (error) { 
+      console.log(error);
+      res.json({
+        message:"Internal Server Error!"
+      })
+    }
+    finally{
+      client.close();
+    }
+})
+router.post('/register', async(req, res)=> {
+    const client = await MongoClient.connect(dbUrl);
+    try {
+      const db = await client.db('b26we');
+      let user = await db.collection('users').findOne({email:req.body.email});
+
+      if(user)
+      {
+        res.json({
+          message:"User with same email exists"
+        })
+      }
+      else{
+        let document = await db.collection('users').insertOne(req.body);
+        res.json({
+          message:"User Registered Successfully!",
+          document
+        })
+      }
+
+    } catch (error) { 
+      console.log(error);
+      res.json({
+        message:"Internal Server Error!"
+      })
+    }
+    finally{
+      client.close();
+    }
+
 });
-router.post('/register',async(req, res)=>{
-  const client = await MongoClient.connect(dburl);
+
+
+router.put('/edit-user/:id',async(req,res)=>{
+  const client = await MongoClient.connect(dbUrl);
   try {
     const db = await client.db('b26we');
-    let user = await db.collection('users').findOne({email:req.body.email})
-  if(user){
-res.json({
-  message:"the email already exist"
-})
-  }else{
-     await db.collection('users').insertOne(req.body)
+    let document = await db.collection('users').findOneAndReplace({_id:mongodb.ObjectId(req.params.id)},req.body)
+    if(document.value)
+    {
+      res.json({
+        message:"Data Changed Successfully!",
+        data:document
+      })
+    }
+    else
+    {
+      res.status(404).json(({
+        message:"Invalid Id"
+      }));
+    }
+
+  } catch (error) { 
+    console.log(error);
     res.json({
-      message:"user Registered Successfully",
-     
+      message:"Internal Server Error!"
     })
   }
-    
-  } catch (error) {
-      console.log(error)
+  finally{
+    client.close();
+  }
+
+})
+
+
+router.delete('/delete-user/:id',async(req,res)=>{
+  const client = await MongoClient.connect(dbUrl);
+  try {
+    const db = await client.db('b26we');
+    let document = await db.collection('users').findOneAndDelete({_id:mongodb.ObjectId(req.params.id)})
+    if(document.value)
+    {
       res.json({
-        message:"Registration failed"
-      })  
-  } finally{
-    client.close()
-  }
-  });
+        message:"Data Deleted Successfully",
+      })
+    }
+    else
+    {
+      res.status(404).json(({
+        message:"Invalid Id"
+      }));
+    }
 
+  } catch (error) { 
+    console.log(error);
+    res.json({
+      message:"Internal Server Error!"
+    })
+  }
+  finally{
+    client.close();
+  }
 
-router.put('/edituser/:id',async(req,res)=>{
-  const client = await MongoClient.connect(dburl);
-try {
-  const db = await client.db('b26we');
-  let document = await db.collection('users').findOneAndReplace({_id:ObjectId(req.params.id)},req.body)
-if(document.value){
-  res.json({
-    message:"Edited successfully",
-    data:document.value
-  })
-}  else{
-res.json({
-  messsage:"ivalid ID",
-  
 })
-  }
-} catch (error) {
-    console.log(error)
-    res.json({
-      message:"data fetching failed"
-    })  
-} finally{
-  client.close()
-}
-})
-router.delete('/deleteuser/:id',async(req,res)=>{
-  const client = await MongoClient.connect(dburl);
-try {
-  const db = await client.db('b26we');
-  let document = await db.collection('users').findOneAndDelete({_id:ObjectId(req.params.id)})
-if(document){
-  res.json({
-    message:"Deleted successfully",
-  })
-}  else{
-res.json({
-  messsage:"ivalid ID"
-  
-})
-  }
-} catch (error) {
-    console.log(error)
-    res.json({
-      message:"data fetching failed"
-    })  
-} finally{
-  client.close()
-}
-})  
 module.exports = router;
